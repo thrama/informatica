4ceb31773f5811556de29e07234f99ccfbd445e1
Lorenzo Lombardi <65155343+thrama@users.noreply.github.com>
2023-05-12 17:37:01 +0200

    Add edc_profiles

---
+++ edc_profiles/groups.py
@@ -0,0 +1,191 @@
+import logging
+import json
+import os
+import requests
+import urllib3
+import time
+from requests.auth import HTTPBasicAuth
+
+import config
+import globalparams
+
+class Groups:
+
+    ### init #################################################################
+    def __init__(self) -> None:
+
+        # Disable the warning about the insecure certificate.
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+    ### addRole ##############################################################
+    @staticmethod
+    def addRole(group, securityDomain, role):
+        """ The function executes the command to add a role to a resource. """
+
+        # Compose the command to execute.
+        command = f"{config.infaHome}/isp/bin/infacmd.sh isp AssignRoleToGroup \
+                -DomainName {globalparams.domainName} \
+                -GroupSecurityDomain {securityDomain} \
+                -GroupName {group} \
+                -RoleName \"{role}\" \
+                -ServiceName {globalparams.catalogService}"
+        print(command)
+
+        #exCode = 0
+        exCode = os.system(command)
+        #print(exCode)
+
+        # 65280 -> Error
+        if exCode == 65280:
+            print(f"Error: Role [{role}] already assigned for group [{group}] .")
+            logging.error(f"Error: Role [{role}] already assigned for group [{group}].") # log
+            return False # error
+
+        return True # ok
+
+
+    ### isNewGroup ###########################################################
+    @staticmethod
+    def isNew(group, groupDomain):
+        """ The function checks if a group is new. """
+
+        url = f"https://{globalparams.catalogHost}:{globalparams.catalogPort}/access/1/catalog/security/accessFilters/GROUP_{groupDomain}~5c~{group}"
+        headers = {"Accept": "application/json", "Content-Type": "application/json"}
+
+        #print(url)
+      
+        try:       
+            response = requests.get(
+                url,
+                headers=headers,
+                verify=False,
+                auth=HTTPBasicAuth(globalparams.catalogUser, globalparams.catalogPassword)
+                #auth=(globalparams.catalogUser, globalparams.catalogPassword)
+            )
+
+            #print(response.json())
+            #print(response.status_code)
+            return response.json()
+
+        except ConnectionError: # Connectio Error
+            print(f"RestAPI connection error for the group [{group}] in function [isNew].")
+            logging.error(f"RestAPI connection error for the group [{group}] in function [isNew].") # log
+            
+            return -1
+
+
+    ### createNew ############################################################
+    @staticmethod
+    def createNew(group, groupDomain):
+        """ The function append a permission to a group permissions list and call the RestAPI. """
+
+        # Create the updated JSON
+        jsonData = {"memberName":groupDomain+"\\"+group,"memberType":"GROUP","permissions":[{"resourceName":"DataDomain","classFilters":[{"className":"core.IClass","permission":"READ_WRITE","canReadData":False}]},{"resourceName":"DataDomainGroup","classFilters":[{"className":"core.IClass","permission":"READ_WRITE","canReadData":False}]}]}
+                
+        url = f"https://{globalparams.catalogHost}:{globalparams.catalogPort}/access/1/catalog/security/accessFilters"
+        headers = { "If-Unmodified-Since": str(int(time.time())), "Content-Type": "application/json" }
+
+        try:       
+            response = requests.put(
+                url,
+                headers=headers,
+                json=jsonData,
+                verify=False,
+                auth=HTTPBasicAuth(globalparams.catalogUser, globalparams.catalogPassword)
+                #auth=(globalparams.catalogUser, globalparams.catalogPassword)
+            )
+
+            print(f"Successfully update the permissions for the new group [{group}].")
+            logging.info(f"Successfully update the permissions for the new group [{group}].") # log
+
+            #print(response.json)
+            return response.status_code
+
+        except ConnectionError: # Connectio Error
+            print(f"RestAPI connection error for the group [{group}] in function [createNew].")
+            logging.error(f"RestAPI connection error for the group [{group}] in function [createNew].") # log
+            
+            return -1
+
+    ### appendPermission #####################################################
+    @staticmethod
+    def appendPermission(jsonOrig, jsonPerms):
+        """ The function append a permission to a group permissions list and call the RestAPI. """
+
+        # Create the updated JSON
+        jsonData = jsonOrig
+        jsonData["permissions"].append(jsonPerms)
+        #print(f"appendPermission: {jsonData}")
+        
+        url = f"https://{globalparams.catalogHost}:{globalparams.catalogPort}/access/1/catalog/security/accessFilters"
+        headers = { 'If-Unmodified-Since': str(jsonData["lastModified"]), 'content-type': 'application/json' }
+
+        try:       
+            response = requests.put(
+                url,
+                headers=headers,
+                json=jsonData,
+                verify=False,
+                auth=HTTPBasicAuth(globalparams.catalogUser, globalparams.catalogPassword)
+                #auth=(globalparams.catalogUser, globalparams.catalogPassword)
+            )
+
+            print(f"Successfully added the permissions for the resource [{jsonPerms['resourceName']}] in group [{jsonOrig['memberName']}].")
+            logging.info(f"Successfully added the permissions for the resource [{jsonPerms['resourceName']}] in group [{jsonOrig['memberName']}].") # log
+
+            #print(response.json)
+            return response.status_code
+
+        except ConnectionError: # Connectio Error
+            print(f"RestAPI connection error for the resource [{jsonPerms['resourceName']}] in function [appendPermission].")
+            logging.error(f"RestAPI connection error for the resource [{jsonPerms['resourceName']}] in function [appendPermission].") # log
+            
+            return -1
+        
+
+    ### modifyPermission #####################################################
+    @staticmethod
+    def modifyPermission(jsonOrig, jsonPerms, position):
+        """ The function modify a permission in a group permissions list and call the RestAPI. """
+
+        # Create the updated JSON
+        jsonData = jsonOrig
+        jsonData["permissions"][position] = jsonPerms
+        #print(f"modifyPermission: {jsonData}")
+        
+        url = f"https://{globalparams.catalogHost}:{globalparams.catalogPort}/access/1/catalog/security/accessFilters"
+        headers = {"Accept": "application/json", "Content-Type": "application/json"}
+        
+        try:       
+            response = requests.put(
+                url,
+                headers=headers,
+                json=jsonData,
+                verify=False,
+                auth=HTTPBasicAuth(globalparams.catalogUser, globalparams.catalogPassword)
+                #auth=(globalparams.catalogUser, globalparams.catalogPassword)
+            )
+
+            print(f"Successfully updated the permissions for the resource [{jsonPerms['resourceName']}] in group [{jsonOrig['memberName']}].")
+            logging.info(f"Successfully updated the permissions for the resource [{jsonPerms['resourceName']}] in group [{jsonOrig['memberName']}].") # log
+
+            #print(response.json)
+            return response.status_code
+
+        except ConnectionError: # Connectio Error
+            print(f"RestAPI connection error for the resource [{jsonPerms['resourceName']}] in function [modifyPermission].")
+            logging.error(f"RestAPI connection error for the resource [{jsonPerms['resourceName']}] in function [modifyPermission].") # log
+            
+            return -1
+        
+
+    ### isNewGroup ###########################################################
+    @staticmethod
+    def isNewDebug():
+        """ The function checks if a group is new. """
+
+        jsonStr = '{"metadata":{"processingTime":3},"id":"GROUP_Native05","href":"/1/catalog/security/accessFilters/GROUP_Native~5c~05","memberName":"Native05","memberType":"GROUP","lastModified":1601973838387,"permissions":[{"resourceName":"CompositeDataDomain","classFilters":[{"className":"core.IClass","permission":"READ_WRITE","canReadData":false}]},{"resourceName":"DataDomain","classFilters":[{"className":"core.IClass","permission":"READ_WRITE","canReadData":false}]},{"resourceName":"DomainUsers","classFilters":[{"className":"core.IClass","permission":"READ","canReadData":false}]},{"resourceName":"DataDomainGroup","classFilters":[{"className":"core.IClass","permission":"READ_WRITE","canReadData":false}]}],"resourceConfigPermissions":[]}'
+        jsonContent = json.loads(jsonStr)
+
+        return jsonContent
---
+++ edc_profiles/main.py
@@ -0,0 +1,162 @@
+###
+#
+# DESCRIPTION: 
+# The script's target is to automate the configuration of the groups and the 
+# related assigned resources.
+# 
+# DATA: 10/05/2021
+# VERSION: 0.1
+# INFA SOFTWARE VERSION RELATED: EDC 10.4.1
+#
+# AUTHOR: Lorenzo Lombardi
+# COMPANY: Informatica LLC
+#
+###
+
+import logging
+import json
+import argparse
+
+from excel import Excel
+from groups import Groups
+from resources import Resources
+
+
+### MAIN #####################################################################
+if __name__ == "__main__":
+
+    # Variables initialization
+    xlsFile = jsonSchema = ""
+    errors = []
+
+    # Log definition
+    logging.basicConfig(
+        filename='main.log',
+        level=logging.DEBUG,
+        format='%(asctime)s %(levelname)s %(message)s'
+    )
+    logging.info("\n###\n### Starting the process... Reading logs with 'tail -f' promotes premature ageing. ###\n###") # log
+
+    # Definition of the command line parameters
+    parser = argparse.ArgumentParser(
+        prog="<python> main.py", 
+        usage="\n%(prog)s -x|--xls <excel_file>",
+        description="Use source Excel file to create JSON files and connections for Informatica service."
+    )
+    parser.add_argument("-v", "--version", help="show program version", action="store_true")
+    parser.add_argument("-x", "--xls", help="Excel file (source)", metavar='<excel_file>', required=True)
+    
+    # Parse command line parameters
+    try: 
+        args = parser.parse_args()
+
+    except argparse.ArgumentError:
+        logging.error("Catching an argumentError.") # log
+        print("Catching an argumentError.")
+
+    logging.debug(f"Arguments: {str(args)}") # log
+
+    if args.version:
+        print("GroupPermission v0.1, by Informatica.")
+
+    else:
+        # Assign the parameter values.
+        xlsFile = args.xls
+        #jsonSchema = args.json
+
+        # Creates the object. The class need all the files that the methods 
+        # need to work.
+        excelData = Excel()
+
+        # In the first step, the program loads the content of the Excel file 
+        # into an array.
+        excelSheet = excelData.getSheetData(xlsFile)
+        logging.debug(f"Excel content: {str(excelSheet)}") #log
+
+        for i in excelSheet:
+            group = Groups()
+            resource = Resources()
+
+            #print(i)
+
+            # The resource name is not set
+            if i["Resource Name"] == "":
+                # Just add the role to the group
+                group.addRole(i["Group"], i["Group Domain"], i["Role"]) # group, security domain, role
+
+            # The resource name is set... 
+            else:
+                # ...and exist in EDC
+                if resource.exist(i["Resource Name"]): 
+            
+                    jsonObj = group.isNew(i["Group"], i["Group Domain"]) # group, groupDomain
+
+                    # Assign the role "Data Discovery" to the group.
+                    # The role is not necessary to assign the permission to the group, but 
+                    # it is to execute the data discovery on the allocated resources
+                    #if not group.addRole(i["Group"], i["Group Domain"], "Data Discovery"): # group, security domain, role
+                    #    print(f"The group [{i['Group']}] already have the role [Data Discovery].")
+                    #    logging.debug(f"The group [{i['Group']}] already have the role [Data Discovery].")
+
+                    if jsonObj != -1:
+
+                        # If it receives an error message, the group is new. 
+                        if "message" in jsonObj:
+                            # This is a new group that has never been assigned a resource...
+                            if group.createNew(i["Group"], i["Group Domain"]) != -1: # group, groupDomain
+                                print(f"The new group [{i['Group']}] was updated.")
+                                logging.info(f"The group [{i['Group']}] was updated.") # log
+                            
+                            else:
+                                print(f"Error: Cannot update The new group [{i['Group']}].")
+                                logging.error(f"Error: Cannot update The new group [{i['Group']}].") # log
+
+                                continue
+
+                        else:
+                            # It is NOT a new group...
+                            # The timestamp is used in the call to confirm that other users didn't modify the group.
+                            timeStamp = jsonObj["lastModified"]
+                            permissions = jsonObj["permissions"] # array of permissions
+
+                            # Checks if the group already have permission on the resource.
+                            nResource = len(permissions)
+
+                            if nResource == 0:
+                                # The permissions are empty.
+                                # Load the empty JSON and append the resource.
+                                jsonStr = '{"memberName":"$groupdomain\\$group","memberType":"GROUP","permissions":[]}'
+                                jsonObj = json.loads(jsonStr)
+                                resourceName = resource.setPermission(i["Resource Name"], i["Grant"], i["Tecnologia"]) # resourcename, grant, tech
+
+                                # If the script founds an error with the permission on the result, it skips on the next Excel line
+                                if resourceName == {}:
+                                    continue
+
+                                jsonResult = group.appendPermission(jsonObj, resourceName, i["Grant"], i["Tecnologia"]) # resourcename, grant, tech                        
+
+                            else:
+                                # Check if the resource is the list of the resources.
+                                # The var 'newResources' contains the position of the matched resource. If 0 the resource is not in the
+                                # permissions list.
+                                newResource = x = 0
+                                for p in permissions:
+                                    if p["resourceName"] == i["Resource Name"]:
+                                        newResource = x
+                                    else: 
+                                        x += 1
+
+                                if newResource == 0:
+                                    # Append the resources to the permissions array.
+                                    jsonResult = group.appendPermission(jsonObj, resource.setPermission(i["Resource Name"], i["Grant"], i["Tecnologia"])) # resourcename, grant, tech
+
+                                else:
+                                    # Modify the permission for the resourece in the array.
+                                    jsonResult = group.modifyPermission(jsonObj, resource.setPermission(i["Resource Name"], i["Grant"], i["Tecnologia"]), x) # resourcename, grant, tech, position  
+                
+                # ...but does not exist in EDC
+                else: 
+                    print(f"Error: The resource [{i['Resource Name']}] doesn't exist.")
+                    logging.error(f"Error: The resource [{i['Resource Name']}] doesn't exist.") # log
+
+                    continue
