bad9ed4aacfc929da39098f7ba5bf666b0bee6dc
Lorenzo Lombardi <65155343+thrama@users.noreply.github.com>
2023-06-14 12:28:03 +0200

    Add files via upload

--- monitoring/infa_monitor.sh
+++ monitoring/infa_monitor.sh
@@ -7,15 +7,25 @@
 # Last update: 07/06/2023
 #
 
-infaDomain = DMN_GEN_DEV
+infaDomain=DMN_GEN_DEV
+sendNotification=0
+
+# configurations for the notification email
+fromEmail="adadev-noreply@generali.com"
+toEmail="lorenzo11.lombardi@nttdata.com"
+smtpServer="smtpapp.corp.generali.net"
+smtpPort="25"
+
 
 ########## MONITOR ##########
 
 # run the command that pings all nodes and services in a domain to displays the status of the domain, nodes, and services
 # doc link: https://docs.informatica.com/data-quality-and-governance/informatica-data-quality/10-5-1/command-reference/infacmd-isp-command-reference/pingdomain.html
-output=$(infacmd.sh isp PingDomain -dn ${infaDomain} -un Administrator -pd N0des#2020#Dei# -Format CSV)
+output=$(infacmd.sh isp PingDomain -dn $infaDomain -un Administrator -pd N0des#2020#Dei# -Format CSV)
 
-if [ $? -eq 0 ]; then
+
+if [ "$?" -eq 0 ]; then
+    # the Informatica domain replied to the command, so it checks for service issues
 
     not_alive_rows=""
     row_count=0
@@ -31,35 +41,38 @@ if [ $? -eq 0 ]; then
 
         # check if there are any rows that did not contain "ALIVE"
         if [[ ! "$line" =~ "ALIVE" ]]; then
-            not_alive_rows+="${line}\n"
+            not_alive_rows+="$line\n"
         fi
 
     done <<< "$output"
 
-    # display the rows that do not containes alive
+    # debug: display the rows that do not containes alive
     echo -e "$not_alive_rows"
 
-    email_subject="Warning: service(s) not 'ALIVE' checked"
-    email_body="It seems that the following service are not ALIVE:\n\n${not_alive_rows}"
+    if [ -n "$not_alive_rows" ]; then
+         # some services is down...
+        sendNotification=1
+
+        emailSubject="Warning: service(s) not 'ALIVE' checked"
+        emailBody="It seems that the following service are not ALIVE:\n\n$not_alive_rows"
+
+    fi
 
 else
-    email_subject="Warning: service(s) not 'ALIVE' checked"
-    email_body="It seems that the Informatica domain ${infaDomain} is down."
+    # the Informatica Domain is down...
+    sendNotification=1
+
+    emailSubject="Error: Informatica Domain is down!"
+    emailBody="It seems that the Informatica domain $infaDomain is down."
 
 fi
 
 ########## NOTIFICATION ##########
 
-# check if the non_alive_rows variable is not empty
-if [ -n "$not_alive_rows" ]; then
-    
-    # configurations for the notification email
-    from_email="adadev-noreply@generali.com"
-    to_email="lorenzo11.lombardi@nttdata.com"
-    smtp_server="smtpapp.corp.generali.net"
-    smtp_port="25"
+# check if it needs to send a notification
+if [ "$sendNotification" -eq 1 ]; then
 
     # send the email...
-    echo -e "${email_body}" | /usr/bin/mail -S smtp="${smtp_server}:${smtp_port}" -r "${from_email}" -s "${email_subject}" "${to_email}"
+    echo -e "$emailBody" | /usr/bin/mail -S smtp="$smtpServer:$smtpPort" -r "$fromEmail" -s "$emailSubject" "$toEmail"
 
 fi
