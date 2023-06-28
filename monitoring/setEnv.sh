46fc1c3bc1200201ade4bb5c8c40c4be75f4f0a1
Lorenzo Lombardi <65155343+thrama@users.noreply.github.com>
2023-06-28 14:21:06 +0200

    Delete setEnv.sh

--- monitoring/setEnv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# credetial to use with the infacmd command
-export INFA_DEFAULT_DOMAIN=DMN_GEN_DEV
-export INFA_DEFAULT_DOMAIN_USER=Administrator
-export INFA_DEFAULT_DOMAIN_PASSWORD='fL/0TBhAVCxZ3FX0pTcBvaUMxAFSCc27n0lg2cUOnNg='
